885647dad8efd1db9861e6f4870a9bb837d55b57
patricioespinozaa <patricio.espinoza.a@ug.uchile.cl>
2024-11-05 02:55:30 -0300

    Documentación Arbol binario clasico

--- T2/src/BinarySearchTree.java
+++ T2/src/BinarySearchTree.java
@@ -1,3 +1,5 @@
+// ABB Clasico
+
 public class BinarySearchTree {
     Node root;
 
@@ -5,33 +7,44 @@ public class BinarySearchTree {
         root = null;
     }
 
-    // Inserta un nuevo nodo en el árbol
+    // ======================== INSERCION EN ABB CLASICO ======================== //
+
+    // Insertar un nuevo nodo en el árbol
     void insert(int key) {
         root = insertRec(root, key);
     }
 
-    Node insertRec(Node root, int key) {
-        if (root == null) {
-            root = new Node(key);
+    Node insertRec(Node root, int key) {                 // Sea x el elemento a insertar
+        if (root == null) {                              // Si la raíz es vacía
+            root = new Node(key);                        // se crea un árbol de un solo nodo que contiene el elemento a insertar
             return root;
         }
-        if (key < root.key)
+
+        // En caso contrario, se compara x con el elemento de la raíz r:
+        if (key < root.key)                              // Si x <r, se realiza el mismo proceso en el subárbol izquierdo.
             root.left = insertRec(root.left, key);
-        else if (key > root.key)
+        else if (key > root.key)                         // Si x >r, se realiza el mismo proceso en el subárbol derechox
             root.right = insertRec(root.right, key);
         return root;
     }
 
-    // Busca un nodo en el árbol
+    // ======================== BUSQUEDA EN ABB CLASICO ======================== //
+
+    // Buscar un nodo en el árbol
     boolean search(int key) {
         return searchRec(root, key) != null;
     }
 
-    Node searchRec(Node root, int key) {
-        if (root == null || root.key == key)
-            return root;
-        if (root.key > key)
+    Node searchRec(Node root, int key) {                //  Sea x el elemento buscado
+        if (root == null || root.key == key)            //  se inicia la búsqueda en la raíz
+            return root;                                // Si el elemento en la raíz es x, se termina la búsqueda con éxito
+
+        // En caso contrario, se compara x con el elemento de la raíz r:
+        if (key < root.key)                             // Si x <r, se realiza el mismo proceso en el subárbol izquierdo.
             return searchRec(root.left, key);
-        return searchRec(root.right, key);
+        return searchRec(root.right, key);              // Si x >r, se realiza el mismo proceso en el subárbol derecho.
+
+        // Si eventualmente el subárbol donde deberíamos seguir la búsqueda es vacío, significa que el elemento
+        // no existe, terminando el proceso de manera infructuosa.
     }
 }
